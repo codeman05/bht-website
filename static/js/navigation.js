58cd4d669a698716507b75eb25b275fe5db2964c
Kenneth Brown <kenneth.brown.cr@gmail.com>
2017-06-28 12:08:34 -0400

    Structural work with layout and navigation menu.

--- static/js/navigation.js
+++ static/js/navigation.js
@@ -1,28 +1,3 @@
-// Variables
-var $nav = $('nav');
-var $photoGallery = $('#photo-gallery');
-
-// Functions
-var mobileMenu = function () {
-  $('#content').css('display', 'flex', 'flex-direction', 'row', 'width', '50%', 'background-color', 'green');
-  $('.nav-wrapper').css('display', 'flex', 'flex-direction', 'column');
-};
-
-$(document).ready(function() {
-  $('#menu-button').sidr();
-});
-
-
-// // Toggles the menu off and on with the press of a button.
-// $('.menu-button').click(function() {
-//   if ( $nav.hasClass('showing-menu') ) {  //Checks to see if the menu is already showing
-//     $nav.removeClass('showing-menu');
-//     $nav.hide();
-//   } else {
-//     $nav.show();  
-//     $nav.addClass('showing-menu');
-//   }
-// });
 
 
 
