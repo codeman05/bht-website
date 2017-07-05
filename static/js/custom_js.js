be1ce560078263e135a243af130c60aa3a68e685
Kenneth Brown <kenneth.brown.cr@gmail.com>
2017-07-04 23:29:05 -0400

    About page layout changes

---
+++ static/js/custom_js.js
@@ -0,0 +1,23 @@
+/***** Navigation *****/
+
+$('#menu-icon').sidr();
+
+
+/***** Slideshow *****/
+var currentIndex = 0;
+var items = $('.photo-gallery div');
+var itemAmt = items.length;
+
+function cyclePhotos () {
+    var item = $('.photo-gallery div').eq(currentIndex);
+    items.hide();
+    item.css('display', 'inline-block');
+}
+
+var autoSlide = setInterval(function () {
+currentIndex += 1;
+if ( currentIndex > itemAmt - 1 ) {
+    currentIndex = 0;
+}
+cyclePhotos();
+}, 3000);
