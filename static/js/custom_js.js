d1faefd483efc1e69558db56fc6f60318c31917e
codeman05 <the.kenny.b@gmail.com>
2017-07-17 20:23:33 -0400

    Fix slideshow issues, clean up code, fix navigation icon.

--- static/js/custom_js.js
+++ static/js/custom_js.js
@@ -1,31 +1,13 @@
-/***** Navigation *****/
-
+/**************************   
+NAVIGATION
+**************************/
 $('.nav-icon').sidr();
 
 
-/***** Slideshow *****/
-var currentIndex = 0;
-var photos = $('.photo-gallery div');
-var photoAmt = photos.length;
 
-function cyclePhotos () {
-    var photo = $('.photo-gallery div').eq(currentIndex);
-    photos.hide();
-    photo.css('display', 'inline-block');
-}
-
-cyclePhotos();
-
-var autoSlide = setInterval(function () {
-currentIndex += 1;
-if ( currentIndex > photoAmt - 1 ) {
-    currentIndex = 0;
-}
-cyclePhotos();
-}, 3000);
-
-
-/***** Contact Form *****/
+/**************************   
+CONTACT US -- FORM 
+**************************/
 $('#contact-submit').click(function () {
     alert('This function is not yet active. Try again at a later date.');
 });
